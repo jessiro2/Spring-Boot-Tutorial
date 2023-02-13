3d752b8f44828b4e8a3d60e516da3bcb21634d82
Jessica Rodrigues-Dlouhy <jessiro2@cisco.com>
2023-02-13 10:44:30 -0500

    part 6,7 and 8, content negotiation

--- src/main/java/com/telusko/demo/dao/AlienRepo.java
+++ src/main/java/com/telusko/demo/dao/AlienRepo.java
@@ -1,12 +1,12 @@
 package com.telusko.demo.dao;
 
 import com.telusko.demo.model.Alien;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
+
 
 import java.util.List;
 
-public interface AlienRepo extends CrudRepository<Alien, Integer>
+public interface AlienRepo extends JpaRepository<Alien, Integer>
 {
 
 
